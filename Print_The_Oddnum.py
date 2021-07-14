cb317e728ec8b28f5df1a2d6496832fe12b0bbb6
saravanaskmayil <85149057+saravanaskmayil@users.noreply.github.com>
2021-07-14 19:31:28 +0530

    Add files via upload

---
+++ Print_The_Oddnum.py
@@ -0,0 +1,28 @@
+# Problem-2: With a single integer as the input, generate the following until a = x [series of numbers as shown in below examples]
+# Output: (examples)
+# 1) input a = 1, then output : 1
+# 2) input a = 2, then output : 1
+# 3) input a = 3, then output : 1, 3, 5
+# 4) input a = 4, then output : 1, 3, 5
+# 5) input a = 5, then output : 1, 3, 5, 7, 9
+# 6) input a = 6, then output : 1, 3, 5, 7, 9
+# .
+# .
+# 7) input a = x, then output : 1, 3, 5, 7, .......
+
+
+# Solution:
+
+a=int(input("Enter the integer:"))
+i=-1
+if(a%2==1):
+    a+=1
+    while((a!=1)):  
+        i+=2
+        print(i)
+        a-=1
+if(a%2==0):
+    while(a!=1):  
+        i+=2
+        print(i)
+        a-=1
